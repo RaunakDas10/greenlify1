a5a737058327398de510d3c247fcea0dc10cbd06
RAUNAK DAS <raunak.msit.ece10@gmail.com>
2025-04-07 00:47:39 +0530

    Add files via upload

---
+++ Pricing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Pricing() {
+  return (
+    <section className="bg-light py-2 border-bottom">
+      <div className="container px-5 my-5">
+        <div className="text-center mb-5">
+          <h2 className="fw-bolder">Pay as you grow</h2>
+          <p className="lead mb-0">With our no hassle pricing plans</p>
+        </div>
+        <div className="row gx-5 justify-content-center"></div>
+      </div>
+    </section>
+  );
+}
+
+export default Pricing;
